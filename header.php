5bd9b8f3a1cc5ea15a53c7c9fc5592046926f4d7
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-11-11 11:28:45 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,24 @@
-
+<header>
+    <h1>
+    <?php
+    if ($path_parts['filename'] == "index") {
+        print 'Gallery';
+    }
+    if ($path_parts['filename'] == "fish") {
+        print 'Fish';
+    }
+    if ($path_parts['filename'] == "mammals") {
+        print 'Mammals';
+    }
+    if ($path_parts['filename'] == "birds") {
+        print 'Birds';
+    }
+    if ($path_parts['filename'] == "misc") {
+        print 'Miscellaneous Animals';
+    }
+    if ($path_parts['filename'] == "form") {
+        print 'Registration';
+    }
+    ?>
+    </h1>
+</header>
