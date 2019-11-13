5107a99d8e737753432210a481619d8b5388ab8c
amtownse <57452781+amtownse@users.noreply.github.com>
2019-11-13 11:39:45 -0500

    Update birds.php

--- birds.php
+++ birds.php
@@ -1,8 +1,8 @@
 <?php
-include ('top.php');
-include nav.php;
-include header.php;
+include ("top.php");
+print PHP_EOL;
 ?>
 
 <h2>Kakapo</h2>
-<p>The Kakapo is a very interesting bird. It is a parrot from New Zealand. 
+<p>The Kakapo is a very interesting bird. It is a parrot from New Zealand. But it is not only a parrot but it is an
+  endangered parrot. Furthermore, it can not fly. It lives its entire life on the ground. 
