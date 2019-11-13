e396901de1c45d8632b23d1abfb45b4fbbd27b55
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-11-13 11:39:33 -0500

    Create frameForPages.php
    : )

---
+++ frameForPages.php
@@ -0,0 +1,27 @@
+<?php
+include("top.php");
+print PHP_EOL;
+?>
+        
+        <figure>
+            <img>
+            <figcaption>Animal 1</figcaption>
+        </figure>
+        <p>
+            THis is epiv placeholder
+        </p>
+        
+        <figure>
+            <img>
+            <figcaption>Animal 2</figcaption>
+        </figure>
+        <p>
+            THis is alsso epiv placeholder!!
+        </p>
+        
+        <?php
+        include("footer.php");
+        print PHP_EOL;
+        ?>
+    </body>
+</html>
