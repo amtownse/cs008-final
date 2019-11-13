34e0a4de5343ea5b25a94a83c5979d714ea231ca
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-11-13 11:10:57 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,1 +1,41 @@
-
+<nav>
+    <ul>
+        <?php
+        print '<li class="';
+        if ($path_parts['filename'] == "index") {
+            print ' activePage ';
+        }
+        print '"><a href="index.php">Home</a></li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "fish") {
+            print ' activePage ';
+        }
+        print '"><a href="fish.php">Recycling</a></li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "mammals") {
+            print ' activePage ';
+        }
+        print '"><a href="mammals.php">Renewable</a></li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "birds") {
+            print ' activePage ';
+        }
+        print '"><a href="birds.php">Survey</a></li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "misc") {
+            print ' activePage ';
+        }
+        print '"><a href="misc.php">Contest</a></li>';
+        
+        print '<li class="';
+        if ($path_parts['filename'] == "form") {
+            print ' activePage ';
+        }
+        print '"><a href="form.php">Contest</a></li>';
+        ?>
+    </ul>
+</nav>
