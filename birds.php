8c4175e987323a3adfb3ec814e857e40f0df8ee5
amtownse <57452781+amtownse@users.noreply.github.com>
2019-11-17 14:22:41 -0500

    Update birds.php

--- birds.php
+++ birds.php
@@ -1,11 +1,3 @@
-<?php
-include ("top.php");
-print PHP_EOL;
-?>
-
-
-<p>The Kakapo is a very interesting bird. It is a parrot from New Zealand. But it is not only a parrot but it is an
-  endangered parrot. Furthermore, it can not fly. It lives its entire life on the ground. 
 
   <?php
 include("top.php");
@@ -17,7 +9,15 @@ print PHP_EOL;
             <figcaption>The Kakapo</figcaption>
         </figure>
         <p>
-            The Kakapo is a very interesting bird. It is a parrot from New Zealand. However it is also very endangered. 
+          The Kakapo is a very interesting bird. It is a nocturnal parrot from New Zealand. However it is also very endangered.
+          Very endangered in this case meaning only about 150-200 birds are still living
+          One of the reasons it is so endangered is that it cannot fly. This makes it very easy for predators to catch it.
+          One might wonder how, evolutionarily a flightless parrot could come to be. Simply put, it had no natural predators
+          in New Zealand until humans came so being flightless would have been ok. However, when humans arrived they brought 
+          with them cats and rats which could easily hunt the Kakapo. The reason these animals could hunt them so easily is 
+          because the Kakapo are stinky and so are their nests. Prior to humans this was ok, as their only predators were 
+          other birds who could not smell and so would have no way of finding the Kakapo, but the mammals that humans brought 
+          had no difficulties finding them. The Kakapo are a very interesting endangered bird.
         </p>
         
         <figure>
@@ -25,7 +25,13 @@ print PHP_EOL;
             <figcaption>Animal 2</figcaption>
         </figure>
         <p>
-            THis is alsso epiv placeholder!!
+          The oilbird is a very interesting bird in its own right. Also, it’s name makes it clear what one of the reasons 
+          it is interesting is. It has a lot of oil. Infact local hunt the young birds and use their oil because when it burns 
+          it does not give off any scent. However, this is one of the less interesting things about this bird. It is a 
+          nocturnal bird that lives its life in caves. At this point one might be able to guess what the next interesting 
+          thing is. It uses echolocation like a bat to find its way around. However, unlike a bat, the Oilbird uses 
+          frequencies in the range of human hearing when it uses echolocation. For echolocation it will sometimes emit 250 
+          clicks per second. It also hovers in the air to pick fruit off of trees. The Oilbird is truly an amazing bird.
         </p>
         
         <?php
