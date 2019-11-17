f188e8826a0b3fd40e229ac84fdd57f440006909
amtownse <57452781+amtownse@users.noreply.github.com>
2019-11-17 13:18:59 -0500

    Update birds.php

--- birds.php
+++ birds.php
@@ -3,6 +3,34 @@ include ("top.php");
 print PHP_EOL;
 ?>
 
-<h2>Kakapo</h2>
+
 <p>The Kakapo is a very interesting bird. It is a parrot from New Zealand. But it is not only a parrot but it is an
   endangered parrot. Furthermore, it can not fly. It lives its entire life on the ground. 
+
+  <?php
+include("top.php");
+print PHP_EOL;
+?>
+        
+        <figure>
+            <img>
+            <figcaption>The Kakapo</figcaption>
+        </figure>
+        <p>
+            The Kakapo is a very interesting bird. It is a parrot from New Zealand. However it is also very endangered. 
+        </p>
+        
+        <figure>
+            <img>
+            <figcaption>Animal 2</figcaption>
+        </figure>
+        <p>
+            THis is alsso epiv placeholder!!
+        </p>
+        
+        <?php
+        include("footer.php");
+        print PHP_EOL;
+        ?>
+    </body>
+</html>
