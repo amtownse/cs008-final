743517608ae799752c21fb36fd5de4f050b1bb54
amtownse <57452781+amtownse@users.noreply.github.com>
2019-11-18 14:53:41 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -11,31 +11,31 @@
         if ($path_parts['filename'] == "fish") {
             print ' activePage ';
         }
-        print '"><a href="fish.php">Recycling</a></li>';
+        print '"><a href="fish.php">Fish</a></li>';
         
         print '<li class="';
         if ($path_parts['filename'] == "mammals") {
             print ' activePage ';
         }
-        print '"><a href="mammals.php">Renewable</a></li>';
+        print '"><a href="mammals.php">Mammals</a></li>';
         
         print '<li class="';
         if ($path_parts['filename'] == "birds") {
             print ' activePage ';
         }
-        print '"><a href="birds.php">Survey</a></li>';
+        print '"><a href="birds.php">Birds</a></li>';
         
         print '<li class="';
         if ($path_parts['filename'] == "misc") {
             print ' activePage ';
         }
-        print '"><a href="misc.php">Contest</a></li>';
+        print '"><a href="misc.php">Misc</a></li>';
         
         print '<li class="';
         if ($path_parts['filename'] == "form") {
             print ' activePage ';
         }
-        print '"><a href="form.php">Contest</a></li>';
+        print '"><a href="form.php">Register</a></li>';
         ?>
     </ul>
 </nav>
