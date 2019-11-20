ec56b995d888619ed855fb8aaa1e7f61884327d5
vlai0 <57453116+vlai0@users.noreply.github.com>
2019-11-19 21:37:52 -0500

    Update fish.php

--- fish.php
+++ fish.php
@@ -5,18 +5,31 @@ print PHP_EOL;
         
         <figure>
             <img>
-            <figcaption>Animal 1</figcaption>
+            <figcaption>A Gulper Eel</figcaption>
         </figure>
         <p>
-            THis is epiv placeholder
+            The Gulper Eel, also known as the Pelican Eel, is a deep-sea fish that is found within the ocean’s temperate and tropical areas.
+            is an interesting and well known fish due to its bizarre appearance.
+            Its head is known to be larger than its body, with a mouth that can be opened wide enough to swallow fish larger than itself. 
+            Despite this, analysis from scientists show that they primarily target smaller crustaceans instead of larger prey. 
+            As it is a deep-sea fish, it is usually found living in depths ranging from 500 to 6,000 feet. 
+            Due to the habitat that these eels live in, it is rare for humans to ever end up seeing one of these creatures naturally. 
+            Because of this, a large amount of what we currently know about them come from eels that were accidently caught by deep-sea fishing nets.
         </p>
         
         <figure>
             <img>
-            <figcaption>Animal 2</figcaption>
+            <figcaption>A Red-lipped Batfish</figcaption>
         </figure>
         <p>
-            THis is alsso epiv placeholder!!
+            The Red-lipped Batfish is another fish that is quite unusual to see. 
+            It is one of the many organisms that were named after scientist Charles Darwin. 
+            This interesting fish is often found at depths of 10 to 200 feet within the waters near the Galapagos Islands, as well as off Peru’s coast.
+            Its habitat is within the Galapagos Island Marine Protected zone which protects the area’s biodiversity from human actions. 
+            The name Red-lipped Batfish was given to it due to its notable red lips. 
+            Besides its appearance, the Batfish is also known for its modified fins which allows for it to sit on the seafloor as well as walk. 
+            As these fish are not good swimmers, they use their dorsal fins to lure in prey which makes it easier for them to hunt. 
+            The Red-lipped Batfish doesn’t have any major predators due to its habitat being so far underwater, but rising sea temperatures might cause problems for the species in the future.
         </p>
         
         <?php
