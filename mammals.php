4438ab9c0140a3cc0634bcc58969d7912f9215e4
vlai0 <57453116+vlai0@users.noreply.github.com>
2019-11-19 22:40:31 -0500

    Update mammals.php

--- mammals.php
+++ mammals.php
@@ -5,10 +5,18 @@ print PHP_EOL;
         
         <figure>
             <img>
-            <figcaption>Animal 1</figcaption>
+            <figcaption>A majestic Jerboa</figcaption>
         </figure>
         <p>
-            THis is epiv placeholder
+            The Jerboa is an interesting rodent known for its kangaroo-esque hopping. 
+            There are many different species of Jerboa, with some experts saying there are up to 30. 
+            These rodents are often found in the deserts of central Asian countries like Mongolia and China as well as being found in Northern Africa. 
+            They are solitary creatures and usually live alone in underground burrows until mating season. 
+            In addition to being able to hop around, they are also known for their ability to run at speeds of up to 15 mph when threatened. 
+            This allows them to quickly escape from their predators which include foxes, snakes and owls. 
+            For food, the Jerboa chooses to eat desert plants and roots. 
+            It doesn’t actually drink water naturally, but rather obtains it through the moisture it finds in its foods. 
+            These rodents can actually be kept as pets, but are not allowed in the United States due to African rodents being banned by the CDC.
         </p>
         
         <figure>
