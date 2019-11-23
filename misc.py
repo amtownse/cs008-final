d2949c7e3061aaf61d998e305f6e8ec16f62d9e9
vlai0 <57453116+vlai0@users.noreply.github.com>
2019-11-22 19:52:17 -0500

    Update misc.py

--- misc.py
+++ misc.py
@@ -4,11 +4,20 @@ print PHP_EOL;
 ?>
         
         <figure>
-            <img>
-            <figcaption>Animal 1</figcaption>
+            <img href='Mata.jpg' alt='A picture of a Mata Mata turtle'>
+            <figcaption><a href='https://animalsking.com/matamata-turtle-the-lifestyle-and-habitat-of-a-turtle-matamata/'>Mata Mata</a></figcaption>
         </figure>
         <p>
-            THis is epiv placeholder
+            The Mata Mata is an interesting turtle that is found throughout the Amazon in northern South America, in countries like Venezuela, Brazil, and Peru.
+            The Mata Mata is well known mostly due to its appearance. 
+            They have a triangular, flattened head with a horn on its long snout as well as a shell with spherical cones projecting from it.
+            These creatures also have a neck that can end up being longer than their main bodies.
+            Mata Matas are solitary creatures and usually do not interact with others of their kind besides mating season. 
+            They reside at the bottom of rivers and swamps yet do not display talent for being able to swim. 
+            Besides hatchlings and younger turtles, most adults won’t leave their section of the stream. 
+            Because of this, Mata Matas have a unique method of hunting that involves "vacuuming" up small fish and invertebrates which allows them to stay stationary.
+            Due to their unique appearance, Mata Matas are popular and are even in the exotic pet trade.
+            <a href='https://nationalzoo.si.edu/animals/matamata-turtle'>Source</a>
         </p>
         
         <figure>
