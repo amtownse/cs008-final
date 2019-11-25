92d32df3f6a8900c1f5892a83eea14d14c4f7d95
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-11-24 19:32:44 -0500

    Create index.php
    Example of srcset and sizes:
    <figure>
        <img srcset="images/earth100.jpg 100w,
                  images/earth.jpg 300w" 
                  sizes="(max-width: 850px) 25vw"
                  src="images/earth.jpg" alt="The Earth">
         <figcaption>Our Earth</figcaption>
    </figure>

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+include ("top.php");
+?>
+        <main>
+            <section>
+                <h2>Mammals</h2>
+                <a href="mammals.php">
+                <h3>Jerboa</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Jerboa.jpg" alt="A Jerboa">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+                
+                <a href="mammals.php">
+                <h3>Maned Wolf</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Maned.jpg" alt="A Maned Wolf">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+            </section>
+            
+            
+            <section>
+                <h2>Birds</h2>
+                <a href="birds.php">
+                <h3>Kakapo</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Kakapo.jpg" alt="A Kakapo">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+                
+                <a href="birds.php">
+                <h3>Oilbird</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Oilbird.jpg" alt="An Oilbird">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+            </section>
+            
+            
+            <section>
+                <h2>Fish</h2>
+                <a href="fish.php">
+                <h3>Gulper Eel</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Gulper.png" alt="A Gulper Eel">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+                
+                <a href="fish.php">
+                <h3>Red-Lipped Batfish</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Batfish.jpg" alt="A Red-Lipped Batfish">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+            </section>
+            
+            
+            <section>
+                <h2>Miscellaneous</h2>
+                <a href="misc.php">
+                <h3>Mata Mata</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Mata.jpg" alt="A Mata Mata">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+                
+                <a href="misc.php">
+                <h3>Wallace's Flying Frog</h3>
+                <figure>
+                    <img srcset=" w,
+                                  w" 
+                        sizes="(max-width: px) vw"
+                        src="images/Frog.jpg" alt="A Wallace's Flying Frog">
+                    <figcaption>caption</figcaption>
+                </figure>
+                </a>
+            </section>
+        </main>
+        <?php
+        include ("footer.php");
+        ?>
+    </body>
+</html>
