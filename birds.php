2160eefcccfca2d3dae5b0f7198c105f02d6fb46
amtownse <57452781+amtownse@users.noreply.github.com>
2019-12-04 14:34:28 -0500

    Update birds.php

--- birds.php
+++ birds.php
@@ -19,11 +19,8 @@ print PHP_EOL;
           other birds who could not smell and so would have no way of finding the Kakapo, but the mammals that humans brought 
           had no difficulties finding them. The Kakapo are a very interesting endangered bird.
         </p>
-        
-        <figure>
-            <img src='images/Oilbird.jpg' alt='Picture of a Oilbird'>
-            <figcaption><a href='https://www.britannica.com/animal/oilbird'>Oilbird</a></figcaption>
-        </figure>
+
+
         <p>
           The oilbird is a very interesting bird in its own right. Also, it’s name makes it clear what one of the reasons 
           it is interesting is. It has a lot of oil. Infact local hunt the young birds and use their oil because when it burns 
@@ -33,6 +30,11 @@ print PHP_EOL;
           frequencies in the range of human hearing when it uses echolocation. For echolocation it will sometimes emit 250 
           clicks per second. It also hovers in the air to pick fruit off of trees. The Oilbird is truly an amazing bird.
         </p>
+        
+        <figure>
+            <img src='images/Oilbird.jpg' alt='Picture of a Oilbird'>
+            <figcaption><a href='https://www.britannica.com/animal/oilbird'>Oilbird</a></figcaption>
+        </figure>
         
         <?php
         include("footer.php");
