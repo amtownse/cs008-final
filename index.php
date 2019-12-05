f272c3b1d021503f6cfa8df4897297cc4148a771
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-12-04 20:03:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,109 +1,87 @@
 <?php
 include ("top.php");
 ?>
-        <main>
+        <main> <!-- Mammals Section -->
             <section>
                 <h2>Mammals</h2>
                 <a href="mammals.php">
                 <h3>Jerboa</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Jerboa.jpg" alt="A Jerboa">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Jerboa.jpg" alt="A Jerboa">
+                    <figcaption>A Jerboa</figcaption>
                 </figure>
                 </a>
                 
                 <a href="mammals.php">
                 <h3>Maned Wolf</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Maned.jpg" alt="A Maned Wolf">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Maned.jpg" alt="A Maned Wolf">
+                    <figcaption>A Maned Wolf</figcaption>
                 </figure>
                 </a>
             </section>
             
-            
+            <!-- Birds Section -->
             <section>
                 <h2>Birds</h2>
                 <a href="birds.php">
                 <h3>Kakapo</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Kakapo.jpg" alt="A Kakapo">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Kakapo.jpg" alt="A Kakapo">
+                    <figcaption>A Kakapo</figcaption>
                 </figure>
                 </a>
                 
                 <a href="birds.php">
                 <h3>Oilbird</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Oilbird.jpg" alt="An Oilbird">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Oilbird.jpg" alt="An Oilbird">
+                    <figcaption>An Oilbird</figcaption>
                 </figure>
                 </a>
             </section>
             
-            
+            <!-- Fish Section -->
             <section>
                 <h2>Fish</h2>
                 <a href="fish.php">
                 <h3>Gulper Eel</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Gulper.png" alt="A Gulper Eel">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Gulper.png" alt="A Gulper Eel">
+                    <figcaption>A Gulper Eel</figcaption>
                 </figure>
                 </a>
                 
                 <a href="fish.php">
                 <h3>Red-Lipped Batfish</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Batfish.jpg" alt="A Red-Lipped Batfish">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Batfish.jpg" alt="A Red-Lipped Batfish">
+                    <figcaption>A Red-Lipped Batfish</figcaption>
                 </figure>
                 </a>
             </section>
             
-            
+            <!-- Misc Section -->
             <section>
                 <h2>Miscellaneous</h2>
                 <a href="misc.php">
                 <h3>Mata Mata</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Mata.jpg" alt="A Mata Mata">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Mata.jpg" alt="A Mata Mata">
+                    <figcaption>A Mata Mata</figcaption>
                 </figure>
                 </a>
                 
                 <a href="misc.php">
                 <h3>Wallace's Flying Frog</h3>
                 <figure>
-                    <img srcset=" w,
-                                  w" 
-                        sizes="(max-width: px) vw"
-                        src="images/Frog.jpg" alt="A Wallace's Flying Frog">
-                    <figcaption>caption</figcaption>
+                    <img src="images/Frog.jpg" alt="A Wallace's Flying Frog">
+                    <figcaption>A Wallace's Flying Frog</figcaption>
                 </figure>
                 </a>
             </section>
+            
+            <!-- Table with facts about animals read from database -->
             <table>
                 <caption>Basic Information</caption>
                 <tr>
@@ -113,6 +91,7 @@ include ("top.php");
                     <th>Length/Height (m)</th>
                     <th>Lifespan (years)</th>
                 </tr>
+                <!-- Get the table from database and read out each piece for the rows-->
                 <?php
                 $sql2 = 'SELECT fldName, fldHabitat, fldWeight, fldLength, fldLifespan FROM `tblAnimalInfo`';
                 $statement2 = $pdo->prepare($sql2);
