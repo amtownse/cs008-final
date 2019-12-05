e347fc49e18730f17139c14c3d4facc9d912205d
Dmelkumo <57453111+Dmelkumo@users.noreply.github.com>
2019-12-04 20:07:48 -0500

    Update top.php

--- top.php
+++ top.php
@@ -1,8 +1,10 @@
 <?php
 $phpSelf = htmlentities($_SERVER['PHP_SELF'], ENT_QUOTES, "UTF-8");
 
+//in order to get filename
 $path_parts = pathinfo($phpSelf);
 
+//connect to database
 $databaseName = 'DMELKUMO_cs008-final';
 $dsn = 'mysql:host=webdb.uvm.edu;dbname=' . $databaseName;
 $dbUserName = "dmelkumo_writer"; 
@@ -13,6 +15,7 @@ $pdo = new PDO($dsn, $dbUserName, $dbPassword);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <!-- Determine the title based on filename -->
     <?php
     if ($path_parts['filename'] == "index") {
         print '<title>Exotic Animal Gallery</title>';
@@ -35,6 +38,7 @@ $pdo = new PDO($dsn, $dbUserName, $dbPassword);
     ?>
     <meta charset="UTF-8">
     <meta name="author" content="David Melkumov, Alexander Townsend, Vincent Lai">
+    <!-- determine description based on filename -->
     <?php
     print '<meta name="description" content="';
     if ($path_parts['filename'] == "index") {
@@ -55,9 +59,13 @@ $pdo = new PDO($dsn, $dbUserName, $dbPassword);
     if ($path_parts['filename'] == "form") {
         print 'A registration form for our weekly emails">';
     }
+    if ($path_parts['filename'] == "sql") {
+        print 'The sql statements used in this assignment">';
+    }
     ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/custom.css?version=1.0" type="text/css">
+    <!-- Give the body an id based on filename to make page specific css adjustments -->
     <?php
     print '<body id="' . $path_parts['filename'] . '">' . PHP_EOL;
     
